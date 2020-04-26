9d2b2217aba3c34eeac4655b05f76d7b69924fed
Amy McGann <mcgannamyl@gmail.com>
2020-04-26 08:01:04 -0400

    new index.html file to remove bootstrap and adding a js file

---
+++ UI/Jchain.js
@@ -0,0 +1,14 @@
+function message() {
+	fetch('http://localhost:8000/add')
+		.then(function(response){
+						
+			return response.text();
+		})
+.then(function(text){
+    console.log(text);
+
+});
+  
+};
+
+
